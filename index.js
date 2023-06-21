28d846016b830870e8c00aea68aa40e10a7b2df4
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-21 07:09:34 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// input.addEventListener('click', true);
 function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', () => {
+    // Event listener logic
+  });
 }
+
+// Call the addingEventListener function
+addingEventListener();
